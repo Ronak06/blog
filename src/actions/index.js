0f2df97e3905aae5e6779d08d3b1417c86f871d2
Ronak Patel <ronak1.patel@ryerson.ca>
2019-03-10 19:56:22 -0400

    added lodash library to utilize lodash's functions to map and get unique userIds from the fetchPosts action creator. Furthermore, created a new action creator called fetchPostsAndUsers which calls the two action creators fetchPosts and fetchUsers and only calls the fetchUser action creator for all unique userIds from posts.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,45 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // Gets all the unique userIds from the 100 comments within redux store by calling getState().posts
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+  // for each of the unique userIds, run the fetchUser action creator and dispatch it
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+/*
+  Makes an asynchronous request to jsonPlaceholder api to
+  get the data from /posts; once it has extracted the data,
+  it will call dispatch and pass the data to the redux store 
+*/
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
+/*
+  Makes asynchronous request to jsonPlacehold api to get the data
+  of a user from /users/${id}; once it has extracted the data,
+  it will call dispatch and pass the data to the redux store
+*/
 export const fetchUser = id => async dispatch => {
   const respose = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: respose.data });
 };
+
+//  _fetchUser will have only one function call per a specific id provided
+//  due to _.memoize, thus for 100 comments, there will only be 10 user calls
+//  made as each user has 10 comments.
+//
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const respose = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: respose.data });
+// });
