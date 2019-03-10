4da01cd4b0e3d538bfa2baca5835d601f1cd8751
Ronak Patel <ronak1.patel@ryerson.ca>
2019-03-10 18:44:37 -0400

    Added usersReducer which will extract the author of the comment's name from JSONPlaceholder API. Added a userHeader component which will display the author's name.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PostList from "./PostList";
+import "../css/App.css";
 
 const App = () => {
   return (
     <div className="ui container">
+      <h1>Blog Post</h1>
       <PostList />
     </div>
   );
